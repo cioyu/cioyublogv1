3244b306ff43ea736dc01e4b3f80b760fb483e89
cioyu <cioyu@qq.com>
2026-03-03 18:51:13 +0800

    咕咕嘎嘎

---
+++ src/App.vue
@@ -0,0 +1,37 @@
+<script setup></script>
+
+<template>
+  <div id="cheader">
+    <h1>Cioyu的个人网页</h1>
+  </div>
+  <p>简介: 网络专业的学生, 喜欢看小说, 偶尔看下番, 玩玩游戏</p>
+  <p>折腾GNU/linux的历程:</p>
+  <div class="text0">
+    <p>Ubuntu : 安装的第一个发行版</p>
+    <p>openEuler : 学校上linux课安装的(虚拟机)</p>
+    <p>archlinux : 看up主Shorin的niri感觉很炫酷, 跟着搭建了</p>
+    <p>fedora : 当时archlinux分区分小了, 于是重装了</p>
+    <p>cachy : gnome桌面我不喜欢, 打算搞安装kde版本的fedora,结果系统启动黑屏,于是换到了cachy</p>
+  </div>
+  <br>
+  <p>目前正在学习web前端(html,css,javascript,vue)</p>
+  <p>未来计划深入学习c,rust,php</p>
+  <br>
+  <p>联系方式:</p>
+  <ul>
+    <li><a href="https://github.com/cioyu">github</a></li>
+    <li><a href="https://space.bilibili.com/1928417915s">bilibili</a></li>
+    <li><a href="https://www.youtube.com/channel/UC_WmZj9i7MZV899g1iunwdA">youtube</a></li>
+    <li><a href="mailto:cioyu32@gmail.com">email(cioyu32@gmail.com)</a></li>
+  </ul>
+</template>
+
+<style scoped>
+#cheader {
+  text-align: center;
+  background-color: antiquewhite;
+}
+.text0 {
+  margin-left: 5%;
+}
+</style>
